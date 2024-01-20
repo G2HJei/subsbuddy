4ae3175e53148ee6b91b74f55937179f722349fb
Boyan Zlatanov <boyan@zlatanov.xyz>
2024-01-20 12:33:19 +0000

    Hello World!

---
+++ src/main/java/xyz/zlatanov/subsbuddy/SubsBuddyApplication.java
@@ -0,0 +1,13 @@
+package xyz.zlatanov.subsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/zlatanov/subsbuddy/SubsBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.zlatanov.subsbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
