73ab1dda1d3cb8556df37765a6aab53711b1709c
Boyan Zlatanov <b.zlatanov@tinqin.com>
2024-01-20 21:07:43 +0000

    Disable DB test

--- src/test/java/xyz/zlatanov/subsbuddy/DatabaseTests.java
+++ src/test/java/xyz/zlatanov/subsbuddy/DatabaseTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.bson.Document;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,7 @@ import org.springframework.data.mongodb.core.query.Query;
 import lombok.val;
 
 @SpringBootTest
+@Disabled // to be tested with deployment on test environment for acceptance testing
 public class DatabaseTests {
 
 	@Autowired
