308e50376bc00abea48097fb2776c85bca7e65e6
Boyan Zlatanov <boyan@zlatanov.xyz>
2024-02-04 00:37:07 +0000

    Add NightwatchJS tests

--- src/test/nightwatch/nightwatch.conf.js
+++ src/test/nightwatch/nightwatch.conf.js
@@ -14,18 +14,15 @@
 module.exports = {
 	// An array of folders (excluding subfolders) where your tests are located;
 	// if this is not specified, the test source must be passed as the second argument to the test runner.
-	src_folders: ['test'],
+	src_folders: ['end2end'],
 
 	// See https://nightwatchjs.org/guide/concepts/page-object-model.html
-	//page_objects_path: ['nightwatch/page-objects'],
 	page_objects_path: [],
 
 	// See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
-	//custom_commands_path: ['nightwatch/custom-commands'],
 	custom_commands_path: [],
 
 	// See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
-	//custom_assertions_path: ['nightwatch/custom-assertions'],
 	custom_assertions_path: [],
 
 	// See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
