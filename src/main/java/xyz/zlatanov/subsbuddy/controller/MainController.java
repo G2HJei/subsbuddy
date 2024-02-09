b9f4d7a08e36bfa9cfe1819acaf9eac19ef2b194
Boyan Zlatanov <boyan@zlatanov.xyz>
2024-02-09 23:12:47 +0000

    Handle file too large errors

--- src/main/java/xyz/zlatanov/subsbuddy/controller/MainController.java
+++ src/main/java/xyz/zlatanov/subsbuddy/controller/MainController.java
@@ -2,10 +2,8 @@ package xyz.zlatanov.subsbuddy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
