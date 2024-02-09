06907659a9356b3ee5389931aecc65fc9ad6845d
Boyan Zlatanov <boyan@zlatanov.xyz>
2024-02-09 21:54:07 +0000

    Enable db sb test

--- src/test/java/xyz/zlatanov/subsbuddy/DatabaseTests.java
+++ src/test/java/xyz/zlatanov/subsbuddy/DatabaseTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.bson.Document;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -18,7 +19,7 @@ public class DatabaseTests {
 	@Autowired
 	MongoTemplate mongo;
 
-	// @Test // to be tested with deployment on test environment for acceptance testing
+	@Test
 	void database_setup_works() {
 		val collection = "db_test";
 		if (mongo.collectionExists(collection)) {
