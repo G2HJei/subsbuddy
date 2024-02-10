b937a8396b81b9bbc01139f48073f400139e4a66
Boyan Zlatanov <boyan@zlatanov.xyz>
2024-02-10 23:22:59 +0000

    Add page model

--- src/main/java/xyz/zlatanov/subsbuddy/service/HomeService.java
+++ src/main/java/xyz/zlatanov/subsbuddy/service/HomeService.java
@@ -1,5 +1,7 @@
 package xyz.zlatanov.subsbuddy.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -8,13 +10,24 @@ import xyz.zlatanov.subsbuddy.command.upload.FileUploadCommand;
 import xyz.zlatanov.subsbuddy.command.upload.UploadFileHandler;
 import xyz.zlatanov.subsbuddy.exception.NonEnglishSubtitleUploaded;
 import xyz.zlatanov.subsbuddy.exception.NotSupportedFileType;
+import xyz.zlatanov.subsbuddy.model.SubtitleFileModel;
+import xyz.zlatanov.subsbuddy.query.availablesubs.AvailableSubsQuery;
+import xyz.zlatanov.subsbuddy.query.availablesubs.AvailableSubsQueryHandler;
 import xyz.zlatanov.subsbuddy.util.ReadUtils;
 
 @Service
 @AllArgsConstructor
 public class HomeService {
 
-	private UploadFileHandler uploadFileHandler;
+	private UploadFileHandler			uploadFileHandler;
+	private AvailableSubsQueryHandler	availableSubsQueryHandler;
+
+	public List<SubtitleFileModel> getModel(String owner) {
+		return availableSubsQueryHandler.list(new AvailableSubsQuery().owner(owner))
+				.result().stream()
+				.map(d -> new SubtitleFileModel(d.id(), d.filename(), d.language()))
+				.toList();
+	}
 
 	public void upload(String fileName, byte[] bytes, String owner) {
 		if (!fileName.endsWith(".srt")) {
