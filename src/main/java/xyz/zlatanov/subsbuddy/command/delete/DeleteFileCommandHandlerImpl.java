dcbf188f6cacd105d5c00da75624293e056923bb
Boyan Zlatanov <boyan@zlatanov.xyz>
2024-03-09 22:34:48 +0000

    Delete translation link with source

--- src/main/java/xyz/zlatanov/subsbuddy/command/delete/DeleteFileCommandHandlerImpl.java
+++ src/main/java/xyz/zlatanov/subsbuddy/command/delete/DeleteFileCommandHandlerImpl.java
@@ -5,16 +5,19 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
 import xyz.zlatanov.subsbuddy.repository.MovieSubtitleRepository;
+import xyz.zlatanov.subsbuddy.repository.TranslationRepository;
 
 @Service
 @AllArgsConstructor
 public class DeleteFileCommandHandlerImpl implements DeleteFileCommandHandler {
 
-	private MovieSubtitleRepository movieSubtitleRepository;
+	private MovieSubtitleRepository	movieSubtitleRepository;
+	private TranslationRepository	translationRepository;
 
 	@Override
 	@Transactional
 	public void execute(DeleteFileCommand command) {
 		movieSubtitleRepository.deleteById(command.id());
+		translationRepository.deleteAllBySourceId(command.id());
 	}
 }
--- src/test/java/xyz/zlatanov/subsbuddy/command/delete/DeleteFileCommandHandlerImplTest.java
+++ src/test/java/xyz/zlatanov/subsbuddy/command/delete/DeleteFileCommandHandlerImplTest.java
@@ -7,15 +7,18 @@ import static org.mockito.Mockito.mock;
 import org.junit.jupiter.api.Test;
 
 import xyz.zlatanov.subsbuddy.repository.MovieSubtitleRepository;
+import xyz.zlatanov.subsbuddy.repository.TranslationRepository;
 
 class DeleteFileCommandHandlerImplTest {
 
-	MovieSubtitleRepository	movieSubtitleRepository	= mock(MovieSubtitleRepository.class);
-	DeleteFileCommandHandler			handler					= new DeleteFileCommandHandlerImpl(movieSubtitleRepository);
+	MovieSubtitleRepository		movieSubtitleRepository	= mock(MovieSubtitleRepository.class);
+	TranslationRepository		translationRepository	= mock(TranslationRepository.class);
+	DeleteFileCommandHandler	handler					= new DeleteFileCommandHandlerImpl(movieSubtitleRepository, translationRepository);
 
 	@Test
 	void deleteFileHandler_deleteFile_returns() {
 		doNothing().when(movieSubtitleRepository).deleteById("test");
+		doNothing().when(translationRepository).deleteAllBySourceId("test");
 		assertDoesNotThrow(() -> handler.execute(new DeleteFileCommand().id("test")));
 	}
 
