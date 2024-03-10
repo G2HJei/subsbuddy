570d3870bf0b631c77430597ea9309378dec8741
Boyan Zlatanov <boyan@zlatanov.xyz>
2024-03-10 14:34:05 +0000

    Refactor translate command

--- src/main/java/xyz/zlatanov/subsbuddy/command/translate/TranslateFileCommandHandlerImpl.java
+++ src/main/java/xyz/zlatanov/subsbuddy/command/translate/TranslateFileCommandHandlerImpl.java
@@ -25,39 +25,51 @@ import xyz.zlatanov.subsbuddy.repository.TranslationRepository;
 @AllArgsConstructor
 public class TranslateFileCommandHandlerImpl implements TranslateFileCommandHandler {
 
-	private MovieSubtitleRepository		movieSubtitleRepository;
-	private TranslationRepository		translationRepository;
-	private ParseLinesQueryHandler		parseLinesQueryHandler;
-	private TranslateTextQueryHandler	translateTextQueryHandler;
-	private AssembleSubsContentQueryHandler assembleSubsContentQueryHandler;
+	private MovieSubtitleRepository			movieSubtitleRepository;
+	private TranslationRepository			translationRepository;
+	private ParseLinesQueryHandler			parseLinesQueryHandler;
+	private TranslateTextQueryHandler		translateTextQueryHandler;
+	private AssembleSubsContentQueryHandler	assembleSubsContentQueryHandler;
 
 	@Async
 	@Override
 	@Transactional
 	public void execute(TranslateFileCommand command) {
 		val sourceSub = movieSubtitleRepository.findById(command.id()).orElseThrow(TranslationException::new);
+		validateSourceSub(sourceSub);
+		val matchedByHashCode = movieSubtitleRepository.findBySourceHashCode(sourceSub.subtitleData().hashCode());
+		if (matchedByHashCode == null) {
+			orchestrateTranslation(sourceSub);
+		} else {
+			linkTranslation(sourceSub, matchedByHashCode);
+		}
+	}
+
+	private void validateSourceSub(MovieSubtitle sourceSub) {
 		if (!translationRepository.findBySourceId(sourceSub.id()).isEmpty()) {
 			throw new TranslationException("This subtitle has already been translated.");
 		}
 		if (sourceSub.language() != EN) {
-			throw new TranslationException("Translation non-English subtitles is not supported");
+			throw new TranslationException("Translating non-English subtitles is not supported");
 		}
-		val matchedByHashCode = movieSubtitleRepository.findBySourceHashCode(sourceSub.subtitleData().hashCode());
-		if (matchedByHashCode == null) {
-			val parsedLines = parseLinesQueryHandler.execute(new ParseLinesQuery().subtitleData(sourceSub.subtitleData()));
-			val translatedSubsLines = translateTextQueryHandler.execute(new TranslateTextQuery().linesList(parsedLines.lineList()));
-			val subsContentFormatted = assembleSubsContentQueryHandler
-					.execute(new AssembleSubsContentQuery().linesList(translatedSubsLines.linesList()));
-			val translatedSub = movieSubtitleRepository.save(new MovieSubtitle()
-					.filename(sourceSub.filename())
-					.language(BG)
-					.subtitleData(subsContentFormatted.content())
-					.sourceHashCode(sourceSub.hashCode()));
-			translationRepository.save(new Translation().sourceId(sourceSub.id()).translationId(translatedSub.id()));
-		} else {
-			if (translationRepository.findBySourceIdAndTranslationId(sourceSub.id(), matchedByHashCode.id()) == null) {
-				translationRepository.save(new Translation().sourceId(sourceSub.id()).translationId(matchedByHashCode.id()));
-			}
+	}
+
+	private void orchestrateTranslation(MovieSubtitle sourceSub) {
+		val parsedLines = parseLinesQueryHandler.execute(new ParseLinesQuery().subtitleData(sourceSub.subtitleData()));
+		val translatedSubsLines = translateTextQueryHandler.execute(new TranslateTextQuery().linesList(parsedLines.lineList()));
+		val subsContentFormatted = assembleSubsContentQueryHandler
+				.execute(new AssembleSubsContentQuery().linesList(translatedSubsLines.linesList()));
+		val translatedSub = movieSubtitleRepository.save(new MovieSubtitle()
+				.filename(sourceSub.filename())
+				.language(BG)
+				.subtitleData(subsContentFormatted.content())
+				.sourceHashCode(sourceSub.hashCode()));
+		translationRepository.save(new Translation().sourceId(sourceSub.id()).translationId(translatedSub.id()));
+	}
+
+	private void linkTranslation(MovieSubtitle sourceSub, MovieSubtitle matchedByHashCode) {
+		if (translationRepository.findBySourceIdAndTranslationId(sourceSub.id(), matchedByHashCode.id()) == null) {
+			translationRepository.save(new Translation().sourceId(sourceSub.id()).translationId(matchedByHashCode.id()));
 		}
 	}
 }
