45aadbdb6e80b87759daf017994a4c5eecb5d69c
Boyan Zlatanov <boyan@zlatanov.xyz>
2025-08-07 15:57:41 +0100

    Some tidying up

--- src/test/java/xyz/zlatanov/subsbuddy/query/translatetext/TranslateTextQueryHandlerImplTest.java
+++ src/test/java/xyz/zlatanov/subsbuddy/query/translatetext/TranslateTextQueryHandlerImplTest.java
@@ -4,8 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import lombok.val;
 import xyz.zlatanov.subsbuddy.connector.translation.TranslationConnector;
@@ -16,196 +19,167 @@ class TranslateTextQueryHandlerImplTest {
 	TranslationConnector		translationConnector	= new CapitalizingTranslationConnector();
 	TranslateTextQueryHandler	handler					= new TranslateTextQueryHandlerImpl(translationConnector);
 
-	@Test
-	void shouldTranslateSingleSentenceEntry() {
-		val translation = handler.execute(new TranslateTextQuery()
-				.linesList(List.of(new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 0))
-						.end(LocalTime.of(0, 0, 1))
-						.text("To be the man you've got to beat the man."))));
-
-		assertEquals(List.of(
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 0))
-						.end(LocalTime.of(0, 0, 1))
-						.text("TO BE THE MAN YOU'VE GOT TO BEAT THE MAN.")),
-				translation.linesList());
+	@ParameterizedTest
+	@MethodSource("subsEntriesArgs")
+	void shouldTranslate(List<SubtitleEntry> linesToTranslate, List<SubtitleEntry> expectedTranslatedLines) {
+		val translation = handler.execute(new TranslateTextQuery().linesList(linesToTranslate));
+		assertEquals(expectedTranslatedLines, translation.linesList());
 	}
 
-	@Test
-	void shouldTranslateTwoEntries() {
-		val translation = handler.execute(new TranslateTextQuery()
-				.linesList(List.of(
-						new SubtitleEntry()
+	public static Stream<Arguments> subsEntriesArgs() {
+		return Stream.of(
+				Arguments.argumentSet("Single sentence",
+						List.of(new SubtitleEntry()
+								.start(LocalTime.of(0, 0, 0))
+								.end(LocalTime.of(0, 0, 1))
+								.text("To be the man you've got to beat the man.")),
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("TO BE THE MAN YOU'VE GOT TO BEAT THE MAN."))),
+
+				Arguments.argumentSet("Two entries",
+						List.of(new SubtitleEntry()
 								.start(LocalTime.of(0, 0, 0))
 								.end(LocalTime.of(0, 0, 1))
 								.text("To be the man you've got to beat the man."),
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 3))
-								.end(LocalTime.of(0, 0, 4))
-								.text("If you smell..."))));
-
-		assertEquals(List.of(
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 0))
-						.end(LocalTime.of(0, 0, 1))
-						.text("TO BE THE MAN YOU'VE GOT TO BEAT THE MAN."),
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 3))
-						.end(LocalTime.of(0, 0, 4))
-						.text("IF YOU SMELL...")),
-				translation.linesList());
-	}
-
-	@Test
-	void shouldTranslateMultipleSentences() {
-		val translation = handler.execute(new TranslateTextQuery()
-				.linesList(List.of(
-						new SubtitleEntry()
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 3))
+										.end(LocalTime.of(0, 0, 4))
+										.text("If you smell...")),
+						List.of(new SubtitleEntry()
 								.start(LocalTime.of(0, 0, 0))
 								.end(LocalTime.of(0, 0, 1))
-								.text("To be the man you've got to"),
+								.text("TO BE THE MAN YOU'VE GOT TO BEAT THE MAN."),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 3))
+										.end(LocalTime.of(0, 0, 4))
+										.text("IF YOU SMELL..."))),
 
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 1))
-								.end(LocalTime.of(0, 0, 2))
-								.text("beat the man. If you smell..."))));
+				Arguments.argumentSet("Two-entry sentence",
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("To be the man you've got to"),
 
-		assertEquals(List.of(
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 0))
-						.end(LocalTime.of(0, 0, 1))
-						.text("TO BE THE MAN YOU'VE GOT TO"),
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 1))
-						.end(LocalTime.of(0, 0, 2))
-						.text("BEAT THE MAN. IF YOU SMELL...")),
-				translation.linesList());
-	}
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 1))
+										.end(LocalTime.of(0, 0, 2))
+										.text("beat the man. If you smell...")),
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("TO BE THE MAN YOU'VE GOT TO"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 1))
+										.end(LocalTime.of(0, 0, 2))
+										.text("BEAT THE MAN. IF YOU SMELL..."))),
 
-	@Test
-	void shouldTranslateTriLineSentence() {
-		val translation = handler.execute(new TranslateTextQuery()
-				.linesList(List.of(
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 0))
-								.end(LocalTime.of(0, 0, 1))
-								.text("To be the man"),
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 1))
-								.end(LocalTime.of(0, 0, 2))
-								.text("you've got to"),
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 2))
-								.end(LocalTime.of(0, 0, 3))
-								.text("beat the man."))));
-		assertEquals(List.of(
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 0))
-						.end(LocalTime.of(0, 0, 1))
-						.text("TO BE THE MAN YOU'VE"),
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 1))
-						.end(LocalTime.of(0, 0, 2))
-						.text("GOT TO BEAT THE"),
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 2))
-						.end(LocalTime.of(0, 0, 3))
-						.text("MAN.")),
-				translation.linesList());
-	}
+				Arguments.argumentSet("Tri-line sentence",
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("To be the man"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 1))
+										.end(LocalTime.of(0, 0, 2))
+										.text("you've got to"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 2))
+										.end(LocalTime.of(0, 0, 3))
+										.text("beat the man.")),
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("TO BE THE MAN YOU'VE"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 1))
+										.end(LocalTime.of(0, 0, 2))
+										.text("GOT TO BEAT THE"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 2))
+										.end(LocalTime.of(0, 0, 3))
+										.text("MAN."))),
 
-	@Test
-	void shouldTranslateTwoSentenceLine() {
-		val translation = handler.execute(new TranslateTextQuery()
-				.linesList(List.of(
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 0))
-								.end(LocalTime.of(0, 0, 1))
-								.text("To be the man you've got to beat the man. If you smell..."))));
-		assertEquals(List.of(
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 0))
-						.end(LocalTime.of(0, 0, 1))
-						.text("TO BE THE MAN YOU'VE GOT TO BEAT THE MAN. IF YOU SMELL...")),
-				translation.linesList());
-	}
+				Arguments.argumentSet("Two-line with special characters ",
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("To be the man you've got"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 1))
+										.end(LocalTime.of(0, 0, 2))
+										.text("to \"beat\" the man. If you smell...")),
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("TO BE THE MAN YOU'VE GOT"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 1))
+										.end(LocalTime.of(0, 0, 2))
+										.text("TO \"BEAT\" THE MAN. IF YOU SMELL..."))),
 
-	@Test
-	void shouldTranslateTwoSentenceLineWithSpecialChars() {
-		val translation = handler.execute(new TranslateTextQuery()
-				.linesList(List.of(
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 0))
-								.end(LocalTime.of(0, 0, 1))
-								.text("To be the man you've got"),
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 1))
-								.end(LocalTime.of(0, 0, 2))
-								.text("to \"beat\" the man. If you smell..."))));
-		assertEquals(List.of(
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 0))
-						.end(LocalTime.of(0, 0, 1))
-						.text("TO BE THE MAN YOU'VE GOT"),
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 1))
-						.end(LocalTime.of(0, 0, 2))
-						.text("TO \"BEAT\" THE MAN. IF YOU SMELL...")),
-				translation.linesList());
-	}
+				Arguments.argumentSet("Two-sentence line",
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("To be the man you've got to beat the man. If you smell...")),
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("TO BE THE MAN YOU'VE GOT TO BEAT THE MAN. IF YOU SMELL..."))),
 
-	@Test
-	void shouldTranslateLongPauseTwoLineSentenceSeparately() {
-		val translation = handler.execute(new TranslateTextQuery()
-				.linesList(List.of(
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 0))
-								.end(LocalTime.of(0, 0, 1))
-								.text("To be the man you've got"),
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 5))
-								.end(LocalTime.of(0, 0, 6))
-								.text("to beat the man. If you smell..."))));
+				Arguments.argumentSet("Two-line sentence with long pause",
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("To be the man you've got"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 5))
+										.end(LocalTime.of(0, 0, 6))
+										.text("to beat the man. If you smell...")),
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("TO BE THE MAN YOU'VE GOT"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 5))
+										.end(LocalTime.of(0, 0, 6))
+										.text("TO BEAT THE MAN. IF YOU SMELL..."))),
 
-		assertEquals(List.of(
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 0))
-						.end(LocalTime.of(0, 0, 1))
-						.text("TO BE THE MAN YOU'VE GOT"),
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 5))
-						.end(LocalTime.of(0, 0, 6))
-						.text("TO BEAT THE MAN. IF YOU SMELL...")),
-				translation.linesList());
-	}
-
-	@Test
-	void shouldReduceLineCountWhenNecessary() {
-		val translation = handler.execute(new TranslateTextQuery()
-				.linesList(List.of(
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 0))
-								.end(LocalTime.of(0, 0, 1))
-								.text("Which means you are going to waste a whole lot of taxpayer money"),
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 1))
-								.end(LocalTime.of(0, 0, 2))
-								.text("trying to prove there is"),
-						new SubtitleEntry()
-								.start(LocalTime.of(0, 0, 2))
-								.end(LocalTime.of(0, 0, 3))
-								.text("no DNA."))));
-
-		assertEquals(List.of(
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 0))
-						.end(LocalTime.of(0, 0, 1))
-						.text("WHICH MEANS YOU ARE GOING TO WASTE A WHOLE LOT OF TAXPAYER MONEY TRYING"),
-				new SubtitleEntry()
-						.start(LocalTime.of(0, 0, 1))
-						.end(LocalTime.of(0, 0, 2))
-						.text("TO PROVE THERE IS NO DNA.")),
-				translation.linesList());
+				Arguments.argumentSet("Reduce line count when necessary",
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("Which means you are going to waste a whole lot of taxpayer money"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 1))
+										.end(LocalTime.of(0, 0, 2))
+										.text("trying to prove there is"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 2))
+										.end(LocalTime.of(0, 0, 3))
+										.text("no DNA.")),
+						List.of(
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 0))
+										.end(LocalTime.of(0, 0, 1))
+										.text("WHICH MEANS YOU ARE GOING TO WASTE A WHOLE LOT OF TAXPAYER MONEY TRYING"),
+								new SubtitleEntry()
+										.start(LocalTime.of(0, 0, 1))
+										.end(LocalTime.of(0, 0, 2))
+										.text("TO PROVE THERE IS NO DNA."))));
 	}
 }
