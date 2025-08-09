8341a2465f4db944f82abb129b89199cb53e84a4
Boyan Zlatanov <boyan@zlatanov.xyz>
2025-08-09 13:29:02 +0100

    Use basic auth for simplicity

---
+++ src/main/java/xyz/zlatanov/subsbuddy/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package xyz.zlatanov.subsbuddy.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Value("${AUTH_KEY}")
+	private String authKey;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests(auth -> auth
+						.anyRequest().authenticated())
+				.httpBasic(basic -> {
+				})
+				.csrf(AbstractHttpConfigurer::disable);
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("subs-buddy")
+				.password(authKey)
+				.roles("ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+}
